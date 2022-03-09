1dc60b626d3e6613e592806a7394710ef70e9823
Angel Arshev <angel.arshev.97@gmail.com>
2022-03-09 12:21:21 +0200

    Adding Columns and fucntionality for them

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
+
 
 @Component({
   selector: 'app-add-item-form',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-item-form.component.scss']
 })
 export class AddItemFormComponent implements OnInit {
+  
+  @Input() item: BudgetItem = new BudgetItem('', 0);
+  @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+  
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm){
+    this.formSubmit.emit(form.value);
+    form.reset();
   }
 
 }
